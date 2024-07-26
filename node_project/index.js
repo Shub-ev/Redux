9a07750d2c5f1e66fcaef5a6b0a872cd5f2b364c
Shub-ev <shubhamg200204@gmail.com>
2024-07-26 17:41:33 +0530

    Create Action Reducer & Initial State

--- node_project/index.js
+++ node_project/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World!");
+// let's create out first Action
+const CAKE_ORDER = 'CAKE_ORDER';
+
+// Action is an Object with 'type' property
+// we can also add more properties if we want
+// {
+//     type: CAKE_ORDER,
+//     quantity: 1,
+// }
+
+// we can also create function which returns Action
+function orderCake(){
+    return
+    {
+        type: CAKE_ORDER,
+        quantity: 1
+    }
+}
+
+// now lets build out initial state (which should be in single object)
+const initialState = {
+    numOfCake: 10,
+    anotherProperty: 0
+}
+
+// now we will create a reducer which is a pure function
+// which accepts 2 parameters and returns new state
+// 1. Initial state
+// 2. Action to be performed
+
+const reducerFunc = (state = initialState, action) => {
+    switch(action.type){
+        case CAKE_ORDER:
+            return {
+                numOfCake: state.numOfCake - 1,
+            }
+        default:
+            return state;
+    }
+}
